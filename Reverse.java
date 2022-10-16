558606dec2be550544ef43827a489adecf265446
Arpita Shinde <73011012+arpitashinde@users.noreply.github.com>
2022-10-16 14:44:32 +0530

    Add files via upload

--- Reverse.java
+++ Reverse.java
@@ -1,16 +1,27 @@
-package com.arps;
+package Arrayprograms;
+
+import java.util.Arrays;
 
 public class Reverse {
     public static void main(String[] args) {
-        int i = 34;
-        int rev = 0;
+        int[] arr = {11,22,3,4,44,55};
+        rev(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
-        while(i>0){
-            int rem = i%10;
-            i /= 10;
-            rev = rev* 10 +rem;
-
+    static void rev(int[] arr){
+        int start =0;
+        int end = arr.length-1;
+        while(start < end){
+            swap(arr,start,end);
+            start++;
+            end--;
         }
-        System.out.println(rev);
+    }
+
+    static void swap(int[] arr, int i1, int i2){
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
     }
 }
