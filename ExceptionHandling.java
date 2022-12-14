1ba174927ccd553553434e9807d9a7f039d5ce98
Arpita Shinde <73011012+arpitashinde@users.noreply.github.com>
2022-12-14 16:07:46 +0530

    Add files via upload

---
+++ ExceptionHandling.java
@@ -0,0 +1,30 @@
+package temp;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 0;
+        try {
+            div(4,0);
+        } catch (Exception e) {
+            System.out.println("cannot be divided by zero");
+        }
+//        catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+    }
+
+
+
+    static void div(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("forced msg: cant divide by zero");
+        }
+        else{
+            System.out.println(a/b);
+        }
+
+    }
+
+
+}
