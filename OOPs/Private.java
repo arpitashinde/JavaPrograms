2cf100e024f8d0161c7da39522d894f60762407f
Arpita Shinde <73011012+arpitashinde@users.noreply.github.com>
2023-02-25 18:24:16 +0530

    Add files via upload

---
+++ OOPs/Private.java
@@ -0,0 +1,21 @@
+package OOPs;
+
+public class Private {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Parent{
+    int date;
+    private int year;
+}
+
+class Child extends Parent{
+    void dis(int da, int ye){
+ this.date= da;
+
+    }
+}
+
+
